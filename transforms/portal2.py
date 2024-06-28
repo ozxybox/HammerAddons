4f20e571a10238388b71a88eead6794e6ad9e0a6
ozxybox <kieferstashu@gmail.com>
2024-06-28 11:38:04 -0500

    chore: Remove undesired entity transforms

--- transforms/portal2.py
+++ transforms/portal2.py
@@ -3,36 +3,6 @@ from srctools import Output, conv_bool, conv_int, VMF
 
 from hammeraddons.bsp_transform import trans, Context
 
-
-@trans('Fix Laser Catcher Skins')
-def laser_catcher_skins(ctx: Context):
-    """Fix Valve's bug where reloading saves causes lasers to get their skin wrong."""
-    for ent in ctx.vmf.by_class['prop_laser_catcher']:
-        if not conv_bool(ent['src_fix_skins'], True):
-            continue
-
-        deact_skin, act_skin = ('2', '3') if ent['SkinType'] == '1' else ('0', '1')
-
-        # Look for outputs which do this already.
-        name = ent['targetname']
-
-        has_act = has_deact = False
-        for out in ent.outputs:
-            if has_act and has_deact:
-                break
-            if out.target == name or out.target == '!self':
-                if out.input.casefold() == 'skin':
-                    if out.params == act_skin:
-                        has_act = True
-                    elif out.params == act_skin:
-                        has_act = True
-
-        if not has_act:
-            ent.add_out(Output('OnPowered', '!self', 'Skin', act_skin))
-        if not has_deact:
-            ent.add_out(Output('OnUnPowered', '!self', 'Skin', deact_skin))
-
-
 def needs_paint(vmf: VMF) -> bool:
     """Check if we have paint."""
     for ent_cls in [
@@ -73,27 +43,3 @@ def force_paintinmap(ctx: Context):
         if conv_int(ctx.vmf.spawn['maxblobcount']) == 0:
             ctx.vmf.spawn['maxblobcount'] = '250'
 
-
-@trans('Precache P2 Light Bridge')
-def precache_light_bridge(ctx: Context):
-    """Ensure light bridges have the particle precached."""
-
-    for bridge in ctx.vmf.by_class['prop_wall_projector']:
-        if conv_bool(bridge['StartEnabled', '0']):
-            return  # Starts on, no need.
-        break
-    else:
-        # No bridges in the map.
-        return
-
-    for part in ctx.vmf.by_class['info_particle_system']:
-        # Check for users already fixing the problem.
-        if part['effect_name'].casefold() == 'projected_wall_impact':
-            return
-
-    ctx.vmf.create_ent(
-        classname='info_particle_system',
-        origin='-15872 -15872 -15872',
-        effect_name='projected_wall_impact',
-        start_active='0',
-    )
